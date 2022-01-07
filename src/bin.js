1d20321c0bbf5976f3b543b6795163cd2a496ba3
Travis Lawrence <travis@instabase.com>
2022-01-07 14:24:07 -0800

    clone repo

--- src/bin.js
+++ src/bin.js
@@ -1,7 +1,19 @@
 #!/usr/bin/env node
+var fs = require("fs");
+const path = require("path");
+const { exec } = require("child_process");
 
 function main() {
-  console.log("Hello from main");
+  if (!fs.existsSync("nodes_stuff")) {
+    fs.mkdirSync("nodes_stuff");
+  }
+
+  exec(
+    `
+      cd nodes_stuff;
+      git clone git@github.com:TravisL12/webpack_boilerplate.git
+    `
+  );
 }
 
 if (require.main === module) {
